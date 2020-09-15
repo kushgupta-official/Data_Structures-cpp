1a883dda48803346193d41b94c4286f2a75480bf
kushgupta-official <kushgupta.potus@gmail.com>
2020-09-16 01:09:16 +0530

    Some unused comments removed

--- Tree/Binary Search Tree/main.cpp
+++ Tree/Binary Search Tree/main.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-void kush_gupta(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	#endif
-}
-
 class tree{
 public:
 	int data;
