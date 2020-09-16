9e86d5303dfe873c2b57547f3419e93b3f059ef5
kushgupta-official <kushgupta.potus@gmail.com>
2020-09-17 01:42:18 +0530

    AVL Tree fully implemented

--- Tree/AVL Tree/avl_tree.cpp
+++ Tree/AVL Tree/avl_tree.cpp
@@ -70,25 +70,94 @@ tree* insert(int value,tree *node){
 	else{
 		return node;		//for same valued
 	}
-	//cout<<node->data<<" "<<value<<endl;
+
 	node->height=1+max(height(node->left),height(node->right));
 
 	int balance=getBalance(node);
 
-	if (balance>1 && node->left->data > value){		//ll
+	if (balance>1 && getBalance(node->left)>=0){		//ll
 		return rightRotate(node);
 	}
 
-	if (balance<-1 && node->right->data < value){	//rr
+	if (balance<-1 && getBalance(node->right)<=0){	//rr
 		return leftRotate(node);
 	}
 
-	if (balance>1 && node->left->data < value){		//lr
+	if (balance>1 && getBalance(node->left)<0){		//lr
 		node->left=leftRotate(node->left);
 		return rightRotate(node);
 	}
 
-	if (balance<-1 && node->right->data > value){
+	if (balance<-1 && getBalance(node->right)>0){	//rl
+		node->right=rightRotate(node->right);
+		return leftRotate(node);
+	}
+
+	return node;
+}
+
+tree *minNode(tree *node){		//returns left most node i.e. minimum valued node in the tree 
+	while(node->left!=NULL){
+		node=node->left;
+	}
+	return node;
+}
+
+tree *deleteNode(int value,tree *node){
+	if (node==NULL){
+		return node;
+	}
+	if (value > node->data){
+		node->right=deleteNode(value,node->right);
+	}
+	else if (value < node->data){
+		node->left=deleteNode(value,node->left);
+	}
+	else{
+		tree *temp;
+		if (node->left==NULL && node->right==NULL){		//if it's a leaf node
+			temp=node;
+			node=NULL;
+		}
+		else if (node->left!=NULL && node->right==NULL){	//if only left child
+			temp=node->left;
+			*node=*temp;
+			free(temp);
+		}
+		else if (node->left==NULL && node->right!=NULL){	//if only right child
+			temp=node->right;
+			*node=*temp;
+			free(temp);
+		}
+		else{		//if both childs are non null
+			temp=minNode(node->right);
+			node->data=temp->data;
+			node->right=deleteNode(node->data,node->right);
+		}
+	}
+
+	if (node==NULL){
+		return node;
+	}
+
+	node->height=1 + max(height(node->left),height(node->right));
+
+	int balance=getBalance(node);
+
+	if (balance>1 && getBalance(node->left)>=0){		//ll
+		return rightRotate(node);
+	}
+
+	if (balance<-1 && getBalance(node->right)<=0){	//rr
+		return leftRotate(node);
+	}
+
+	if (balance>1 && getBalance(node->left)<0){		//lr
+		node->left=leftRotate(node->left);
+		return rightRotate(node);
+	}
+
+	if (balance<-1 && getBalance(node->right)>0){	//rl
 		node->right=rightRotate(node->right);
 		return leftRotate(node);
 	}
@@ -108,10 +177,14 @@ void inorder(tree *t){
 }
 
 int main(){
-	int arr[]={10,20,30,40,50,25};
+	int arr[]={4,3,7,5,6,1,2,8,4};
 	for (int i=0;i<sizeof(arr)/sizeof(int);i++){
 		root=insert(arr[i],root);
 	}
 	cout<<"Inorder : ";
 	inorder(root);
+	root=deleteNode(4,root);
+	cout<<"\nInorder : ";
+	inorder(root);
+	return 0;
 }
